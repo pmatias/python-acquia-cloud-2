55c862092ad794db9ab238afc7aac26df4bb03f2
Pablo Fabregat <pablo@ceruleanhq.com>
2019-07-16 10:06:50 +0200

    Adding Python 3.7 to the testing list.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__name__), 'README.md')) as f:
 
 setup(
     name='acapi2',
-    version='2.0.0a6',
+    version='2.0.0b1',
     url='https://github.com/pmatias/python-acquia-cloud-2',
     download_url='https://pypi.org/project/acapi2',
     license='MIT',
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
     ],
     platforms='any',
@@ -34,7 +35,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'http-hmac-python==2.4.0',
-        'requests==2.20.0',
+        'requests=>2.20.0,<=2.23.0',
         'requests-cache==0.4.13',
         'setuptools>=38.5'
     ]
